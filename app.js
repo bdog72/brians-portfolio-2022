2ef5474e1052309ae43d3dbb91ef1f987ad6d698
Brian Bycynski <high5bri@hotmail.com>
2022-03-07 02:51:45 -0500

    Portfolio

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 //
 //
 
-console.log(123);
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
